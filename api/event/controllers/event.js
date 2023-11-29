125d1bef109b30b8bdc04abd5b1afef3c37050e1
Roman Štefek <roman@stefkovi.info>
2023-11-29 12:11:28 +0100

    První verze exportu

--- api/event/controllers/event.js
+++ api/event/controllers/event.js
@@ -5,4 +5,39 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    export: async (ctx, next) => {
+
+        var moment = require('moment');
+
+        //let {transId, refId, status} = ctx.request.body
+        
+        const events = await strapi.services.event.find({ _limit: -1 });
+    
+        ctx.set('Content-Type', 'text/csv');
+
+        var out = "";
+        events.forEach(el => {
+            var row = [];
+            var dtm = moment(el.happened);
+            row.push(dtm.format("DD.MM.YYYY"));
+            row.push(dtm.format("HH:mm:ss"));
+            row.push(el.position.lat);
+            row.push(el.position.lon);
+            var regCells = el.cells.filter(c => c.registered == true)
+            if(regCells.length > 0)  {
+                var rc = regCells[0];                
+                const lac = Math.floor(rc.identity.cid / 256);
+                row.push(rc.identity.cid - (lac*256));
+                row.push(lac);
+                row.push(rc.identity.mnc);
+                row.push(rc.identity.cid);
+            }
+            
+            out += row.join(";")+"\n";
+        });
+        
+        //return out;
+        return out;
+    }
+};
